224d165de852a51b5a0ac89b476fb5a80d4289c0
qiyunkong <2572922513@qq.com>
2021-11-02 17:04:23 +0800

    布局完成

--- gulp/src/js/main.js
+++ gulp/src/js/main.js
@@ -7,8 +7,59 @@ $(function(){
 
 // nav 
 $(function(){
-  $('nav').singlePageNav();
+  $('nav').singlePageNav({offset:130});
 });
+
+
+// 
+$(function(){ 
+  $(".nav-list .nav-item a").click(function(){
+    console.log($(this))
+  })
+  $(".arrowhead").click(function(){
+    $(".nav-list .nav-item a").eq(4).click()
+  })
+  var $position = $(window).scrollTop();
+
+   // 封装防抖函数
+  function debounce (fn,delay){
+    let time;
+    let debounced = function(){
+      let That = this
+      clearTimeout(time)
+      time = setTimeout(function (){
+        fn.call(That);
+      },delay)
+    }
+    return debounced
+  }
+  var time = 400;
+  var off4 = true;
+  $(window).scroll(function() {
+
+    var $scroll = $(window).scrollTop();
+    var $swiper = $(".swiper");
+    var $introduce = $('.introduce');
+    var $equipment = $('.equipment');
+    if($scroll > $position) {
+      if( $scroll>=200 && $scroll<=1000 ){
+        setTimeout(function(){
+          $(window).scrollTop(1000)
+        },10)
+      }
+
+      // console.log('scrollDown');
+      // console.log("$swiper.offset().top",$swiper.offset().top)
+      // console.log("$introduce.offset().top",$introduce.offset().top)
+      // console.log($introduce.offset().top - $scroll)
+    } else {
+      console.log('scrollUp');
+      console.log($equipment.offset().top - $scroll)
+    }
+    $position = $scroll;
+  });
+});
+
 
 // init
 getJSON('/db/category01.json').then(function(json){
